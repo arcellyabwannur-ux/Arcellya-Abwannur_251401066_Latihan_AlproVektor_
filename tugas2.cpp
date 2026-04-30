f9613978541185fc9aef45afcf20a827a2476be6
arcellyabwannur-ux <arcellyabwannur@gmail.com>
2026-04-30 13:47:23 +0700

    Add files via upload

---
+++ tugas2.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+int nilaiTerbesar(vector<int> &nilai, int n) 
+{ 
+// basis 
+    if (n == 1) 
+        return nilai[0]; 
+// rekursif 
+    int maxSebelumnya = nilaiTerbesar(nilai, n - 1); 
+    if (nilai[n - 1] > maxSebelumnya) 
+        return nilai[n - 1];
+    else 
+        return maxSebelumnya; 
+} 
+
+int main() 
+{ 
+    vector<int> data = {70, 88, 92, 65}; 
+    cout << "Nilai terbesar = " << nilaiTerbesar(data, data.size()) << endl; 
+    return 0;
+}
